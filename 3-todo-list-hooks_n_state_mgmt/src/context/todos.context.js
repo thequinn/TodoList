fb646606d7dceac0a7878868b8ad62be21db2bb1
thequinn <annasun.usa@gmail.com>
2020-08-13 20:20:25 -0700

    Using Todo Reducer in other components

--- 3-todo-list-hooks_n_state_mgmt/src/context/todos.context.js
+++ 3-todo-list-hooks_n_state_mgmt/src/context/todos.context.js
@@ -1,5 +1,6 @@
-import React, { createContext } from 'react';
-import useTodoState from "./../hooks/useTodoState";
+import React, { createContext, useReducer } from 'react';
+//import useTodoState from "./../hooks/useTodoState";
+import todoReducer from "../reducers/todo.reducer.js";
 
 const defaultTodos = [
   { id: 1, task: "Mow the lawn using goats", completed: false },
@@ -10,10 +11,10 @@ export const TodosContext = createContext();
 
 export function TodosProvider(props) {
   //const todoStuff = useTodoState(defaultTodos);
-  const []
+  const [todos, dispatch] = useReducer({todoReducer, defaultTodos});
 
   return (
-    <TodosContext.Provider value={todoStuff}>
+    <TodosContext.Provider value={todos, dispatch}>
       {props.children}
     </TodosContext.Provider>
   );
--- 3-todo-list-hooks_n_state_mgmt/src/reducers/todo.reducer.js
+++ 3-todo-list-hooks_n_state_mgmt/src/reducers/todo.reducer.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import uuid from "uuid/v4";
 
 function reducer(state, action) {
   switch (action.type) {
     case 'ADD':
-      return [...state, { id: uuid(), task: newTodoText, completed: false }];
+      return [...state, { id: uuid(), task: action.newTodoText, completed: false }];
     case 'REMOVE':
-      return state.filter(todo => todo.id !== todoId);
+      return state.filter(todo => todo.id !== action.todoId);
     case 'TOGGLE':
       return state.map(todo =>
-        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
+        todo.id === action.todoId ? { ...todo, completed: !todo.completed } : todo
       );
     case 'EDIT':
       return state.map(todo =>
-        todo.id === todoId ? { ...todo, task: newTask } : todo
+        todo.id === action.todoId ? { ...todo, task: action.newTask } : todo
       );
     default:
       return state;
